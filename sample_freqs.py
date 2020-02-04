cba8479fd8ef2d11c3e22e086f9e5cd01819109d
Ike <ikescode@gmail.com>
2020-02-03 16:45:16 -0800

    add sample frequencies file

---
+++ sample_freqs.py
@@ -0,0 +1,28 @@
+from random import randint
+text = 'one fish two fish red fish blue fish'
+histogram = {'one': 1, 'fish': 4, 'two': 1,
+               'red': 1, 'blue': 1}
+
+def sample(histogram):
+  count_sum = 0
+  start = 0
+  end = 0
+  
+  for word in histogram.keys():
+    count_sum += histogram[word]
+
+  rand_index = randint(0, count_sum - 1)
+  
+  for word, count in histogram.items():
+    end = start + count
+    
+    if rand_index in range(start+end):
+      return word
+    else:
+      start = end
+
+
+i=0     
+while i < 1000:
+  print(sample(histogram))
+  i+=1
